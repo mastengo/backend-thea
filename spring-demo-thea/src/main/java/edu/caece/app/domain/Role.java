19a6518ebdf94cdc4e411bf701a78b65314ee55e
Pamela Alvarez <alvpamela47@gmail.com>
2020-03-24 11:20:55 -0300

    arreglo import

--- spring-demo-thea/src/main/java/edu/caece/app/domain/Role.java
+++ spring-demo-thea/src/main/java/edu/caece/app/domain/Role.java
@@ -3,7 +3,6 @@ package edu.caece.app.domain;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
